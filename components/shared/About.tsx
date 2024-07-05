9be525e921c311435db415188b61d061f60d3494
Sundaresan <sundaresanv2004@gmail.com>
2024-07-05 11:11:10 +0530

    Update-0.2

--- components/shared/About.tsx
+++ components/shared/About.tsx
@@ -9,12 +9,10 @@ const About = () => {
                 <div className={`${styles.paragraph} max-w-full mt-5 font-Space leading-loose text-black space-y-3 text-lg`}>
                     <p>
                         I’m a BE CSE student based in Chennai, India, with a deep passion for machine learning (ML) and
-                        deep learning (DL). My journey in technology has been an emotional roller coaster 🎢, filled with
+                        deep learning (DL). My journey in technology has been an emotional roller coaster, filled with
                         building ML and DL models and crafting innovative software solutions in both web development and
-                        AI.
-                        My decision to specialize in AI and ML comes from a profound belief in their transformative
-                        power
-                        🌟.
+                        AI. My decision to specialize in AI and ML comes from a profound belief in their transformative
+                        power.
                     </p>
                     <p>
                         Witnessing the disruptive potential and increasing significance of these technologies across
@@ -23,14 +21,13 @@ const About = () => {
                     </p>
                     <p>
                         In pursuit of specialization, I’m particularly drawn to the intricacies of machine learning,
-                        natural language processing (NLP), and computer vision 🧠👁️. I envision a future where these
-                        disciplines redefine technological innovation. Through hands-on projects and continuous learning
-                        📚,
-                        I strive to stay abreast of industry trends and contribute meaningfully to advancing
+                        natural language processing (NLP), and computer vision. I envision a future where these
+                        disciplines redefine technological innovation. Through hands-on projects and continuous
+                        learning, I strive to stay abreast of industry trends and contribute meaningfully to advancing
                         AI-driven solutions.
                     </p>
                     <p>
-                        Excited to be on this journey and looking forward to the future of AI! 🚀
+                        Excited to be on this journey and looking forward to the future of AI!
                     </p>
                 </div>
             </div>
